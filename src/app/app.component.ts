a84eb3d5b4e9a5a3a20466f89ea00f2420bbfa2b
William-Ro <william.rodriguez.rocha@est.una.ac.cr>
2023-10-02 09:49:56 -0600

    verificion de environment actual

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   constructor(private router: Router) {}
+
+  ngOnInit() {
+    if (environment.production) {
+      console.log('Estamos en producción');
+    } else {
+      console.log('Estamos en desarrollo');
+    }
+  }
 
   shouldShowSidenav(): boolean {
     // Obtén la ruta actual
