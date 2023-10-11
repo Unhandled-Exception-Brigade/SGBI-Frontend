de77e09538d0d66e320a251fc632a7d6dc8a8f2f
Alvaro Herrera <alvaro.herrera.chaves@est.una.ac.cr>
2023-10-10 19:56:26 -0600

    unas validaciones. Aun faltan

--- src/app/components/features/mantenimiento/mantenimiento.component.ts
+++ src/app/components/features/mantenimiento/mantenimiento.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { NgToastService } from 'ng-angular-popup';
 import { AuthService } from 'src/app/services/auth.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 declare var $: any; // Declara jQuery para su uso en TypeScript
 
@@ -11,10 +12,19 @@ declare var $: any; // Declara jQuery para su uso en TypeScript
   templateUrl: './mantenimiento.component.html',
   styleUrls: ['./mantenimiento.component.css'],
 })
-export class MantenimientoComponent implements OnInit, AfterViewInit {
+export class MantenimientoComponent implements OnInit {
   public rol: string = '';
 
+  loginForm= new FormGroup({
+    montoExonerar: new FormControl('',[Validators.required, Validators.email])
+  });
+  
+  getMontoExonerar(){
+    return this.loginForm.get('montoExonerar')
+  }
+  
   constructor(
+    private fb: FormBuilder,
     private usuarioService: UsuarioService,
     private auth: AuthService,
     private toast: NgToastService,
@@ -22,6 +32,7 @@ export class MantenimientoComponent implements OnInit, AfterViewInit {
   ) {}
 
   ngOnInit() {
+
     this.usuarioService.getRolUsuario().subscribe((val) => {
       const rolDelToken = this.auth.obtenerRolDelToken();
       this.rol = val || rolDelToken;
@@ -39,12 +50,42 @@ export class MantenimientoComponent implements OnInit, AfterViewInit {
     }
   }
 
-  ngAfterViewInit() {
-    // Esto se ejecutará después de que la vista se haya inicializado completamente
-    $(".yearpicker").yearpicker({
-      year: 2021,
-      startYear: 2019,
-      endYear: 2050,
+  private markFormGroupTouched(formGroup: FormGroup) {
+    Object.values(formGroup.controls).forEach((control) => {
+      if (control instanceof FormGroup) {
+        this.markFormGroupTouched(control);
+      } else {
+        control.markAsTouched();
+      }
     });
+  }
+
+  onLogin(){
+    this.markFormGroupTouched(this.loginForm);
+    if(this.loginForm.valid){
+      console.log('valido');
+    }else{
+      console.log('Formulario inválido');
+    }
+  }
+
+  enviar(){
+    console.log('sdsd');
+  }
+
+  obtenerErrorCampoMonto() {
+    
+    const campo = this.loginForm.get('montoExonerar');
+
+    if (campo?.hasError('required')) {
+      return 'El monto es requerido';
+    }
+
+    if (campo?.hasError('email')) {
+      return 'Formato invalido';
+    }
+    
+    return '';
+    
   }
 }
