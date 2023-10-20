92719a0fd9845782451cb769d6208739efe7a012
JRN-19 <jcesecurity@protonmail.com>
2023-10-19 22:30:25 -0600

    no guardar la edicion a cancelar

--- src/app/components/modal-informacion-usuario/modal-informacion-usuario.component.ts
+++ src/app/components/modal-informacion-usuario/modal-informacion-usuario.component.ts
@@ -1,9 +1,9 @@
 import { Component, Input } from '@angular/core';
-import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { EditarUsuarioService } from 'src/app/services/editar-usuario.service';
-import { UsuarioService } from "src/app/services/usuario.service";
-import { ApiService } from "src/app/services/api.service";
-import { AuthService } from "src/app/services/auth.service";
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { ApiService } from 'src/app/services/api.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-modal-informacion-usuario',
@@ -13,18 +13,9 @@ import { AuthService } from "src/app/services/auth.service";
 export class ModalInformacionUsuarioComponent {
   @Input() usuario: any;
   public usuarios: any = [];
-  public estado: string = "";
+  public rol: string = '';
   selectedRole: string;
   estadoSeleccionado: string;
-
-  ngOnInit() {
-    this.selectedRole = this.usuario.rol; // Inicializa el valor de selectedRole con el rol del usuario.
-    this.estadoSeleccionado = this.usuario.estaInactivo;
-
-    this.api.obtenerUsuarios().subscribe((res) => {
-      this.usuarios = res;
-    });
-  }
 
   constructor(
     public activeModal: NgbActiveModal,
@@ -34,8 +25,23 @@ export class ModalInformacionUsuarioComponent {
     private usuarioService: UsuarioService
   ) {}
 
+  ngOnInit() {
+    this.selectedRole = this.usuario.rol; // Inicializa el valor de selectedRole con el rol del usuario.
+    this.estadoSeleccionado = this.usuario.estaInactivo;
+
+    this.usuarioService.getRolUsuario().subscribe((val) => {
+      const rolDelToken = this.auth.obtenerRolDelToken();
+      this.rol = val || rolDelToken;
+    });
+  }
+
   closeModal() {
     this.activeModal.close();
+    this.recargarPagina();
+  }
+
+  recargarPagina() {
+    window.location.reload();
   }
 
   cambiarRol(nuevoRol: string) {
